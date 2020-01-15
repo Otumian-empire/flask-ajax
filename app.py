e8f978fcc147a95298720f0fbbff92cb7ba2d03b
otumian <popecan1000@gmail.com>
2020-01-15 11:30:47 +0000

    backend added to insert data into the database

--- app.py
+++ app.py
@@ -10,15 +10,28 @@ DATABASE_NAME = "flask_ajax_db"
 def add_numbers():
 
     status = 0
+    result = "Provide first or last name"
 
     first_name = request.form.get('firstName', '')
     last_name = request.form.get('lastName', '')
 
+    try:
+        conn = sqlite3.connect(DATABASE_NAME)
+        cur = conn.cursor()
+        result = conn.execute(
+            "INSERT INTO `info`(`firstname`, `lastname`) VALUES(?, ?)", (first_name, last_name)).rowcount
+
+        conn.commit()
+
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+        conn.close()
+
     if first_name or last_name:
         status = 1
-        result = f"{first_name}  {last_name}"
-    else:
-        result = "Provide first or last name"
+        result = f"{first_name} {last_name}"
 
     response_object = {
         'status': status,
--- static/app.js
+++ static/app.js
@@ -14,24 +14,51 @@ $(function () {
 
                 if (response.response_object['status'] === 1) {
                     $("#msg").text('');
-                    $("#result").text(response.response_object['result']);
+                    add_item(response.response_object['result'])
+                    // $("#result").text(response.response_object['result']);
                 } else {
-                    $("#result").text('');
+                    // $("#result").text('');
                     $("#msg").text(response.response_object['result']);
                 }
 
                 $('#firstName').val('')
                 $('#lastName').val('')
 
-
             },
             error: function (error) {
                 $('#msg').text(error.response_object);
                 // console.log(error.response_object['result'])
             }
-
         })
 
     })
 
+
+    /**
+     * add an item to the list
+     */
+    function add_item(data) {
+
+        var span = $('<span></span>')
+        span.text(data)
+
+        var btn = $('<button></button>')
+        btn.text('close')
+        btn.addClass('close-btn')
+
+        var clr = $('<div></div>')
+        clr.addClass('clr')
+
+        var li = $('<li></li>')
+        li.append(span)
+        li.append(btn)
+        li.append(clr)
+
+        $('#list').prepend(li)
+
+        $('.close-btn').on('click', function () {
+            // make request to remove item from the database
+            $(this).parent().remove()
+        })
+    }
 })
